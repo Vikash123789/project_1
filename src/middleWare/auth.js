20fd7f0ff64c973fa641261c457ec809d1ef5ac9
Vikash123789 <99880930+Vikash123789@users.noreply.github.com>
2022-03-21 09:17:16 +0530

    Update auth.js

--- src/middleWare/auth.js
+++ src/middleWare/auth.js
@@ -24,31 +24,8 @@ const authentication = function (req, res, next) {
 }
 
 
-// const authorization = function (req, res, next) {
-//     try {
-//         let token = req.headers["x-api-key"]
-//         let decodedToken = jwt.verify(token, "functionUp")
-
-//         let data2 = decodedToken.authorId;
-//         console.log(data2)
-//         data1 = req.params.blogId;
-//         if(!data1){
-//             res.status(404).send({status:false,msg:"authorId is required"})
-//         }
-//         console.log(data1);
-//         if (data1 !== data2) {
-//             return res.status(401).send({ status: false, msg: 'Unauthorized "Cannot be access Other"s Data' })
-//         }
-//         next()
-//     }
-
-//     catch (error) {
-//         res.status(500).send({ status: false, error: error.message })
-//     }
-// }
 
 
 
 
 module.exports.authentication = authentication;
-// module.exports.authorization = authorization;
